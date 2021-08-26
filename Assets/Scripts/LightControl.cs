131935189a246ad335ffbbd0a8f0958fda2cdc8b
om3l3tte <jakrinsiri@hotmail.com>
2021-08-25 21:22:23 -0400

    v1

---
+++ Assets/Scripts/LightControl.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightControl : MonoBehaviour
+{
+    [SerializeField]
+    private Slider LightSlider;
+    [SerializeField]
+    private float startValue = 20.0f;
+    [SerializeField]
+    private float endValue = 160.0f;
+   
+
+    private float lightAngle = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lightAngle = Mathf.Lerp(startValue, endValue, LightSlider.value);
+        gameObject.transform.localEulerAngles = new Vector3(lightAngle, 0, 0);
+    }
+}
